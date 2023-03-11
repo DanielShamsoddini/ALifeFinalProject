3637a6b407084f16eff80a7e33bcd7951b68b1f2
DanielShamsoddini <dshamsoddini@gmail.com>
2023-03-11 14:57:57 -0600

    final project repo created

---
+++ constants.py
@@ -0,0 +1,15 @@
+import math
+randomSeed = None
+minsize = 0.3
+maxsize = 0.7
+randlen = 12
+odds = 3
+numberOfGenerations = 10
+populationSize = 20
+amplitudeB = math.pi/4.0
+frequencyB = 5
+phaseOffsetB = 0
+mjrange = 0.25
+
+numMotorNeurons = 2
+numSensorNeurons = 1
