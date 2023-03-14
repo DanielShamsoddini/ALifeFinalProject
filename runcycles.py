a27ab15f04ba91e50212ea4f45b6028d889506b2
DanielShamsoddini <dshamsoddini@gmail.com>
2023-03-14 17:05:52 -0500

    final data

--- runcycles.py
+++ runcycles.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-randlist = [random.randint(0,((2**25)-1)) for a in range(7)]
+randlist = [random.randint(0,((2**30)-1)) for a in range(2)]
 
 for a in randlist:
-    os.system("python3 runsnakemake.py" + " HYPOneConnected"+str(a)+"Seed" + " " + str(a) )
+    os.system("python3 runsnakemake.py" + " ControlAllConnected"+str(a)+"Seed" + " " + str(a) )
